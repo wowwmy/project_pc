3151555488ba7b03663e312ac29693a033bfb829
wowwmy <2695327768@qq.com>
2023-03-02 20:00:36 +0800

    first

---
+++ 线程池YouTube/ytb_params.py
@@ -0,0 +1,75 @@
+headers = {
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36",
+}
+jsons = {
+    "context": {
+        "adSignalsInfo": {
+            "params": [{"key": "dt", "value": "1676274545671"},
+                       {"key": "flash", "value": "0"},
+                       {"key": "frm", "value": "0"},
+                       {"key": "u_tz", "value": "480"},
+                       {"key": "u_his", "value": "5"},
+                       {"key": "u_h", "value": "864"},
+                       {"key": "u_w", "value": "1536"},
+                       {"key": "u_ah", "value": "834"},
+                       {"key": "u_aw", "value": "1536"},
+                       {"key": "u_cd", "value": "24"},
+                       {"key": "bc", "value": "31"},
+                       {"key": "bih", "value": "731"},
+                       {"key": "biw", "value": "670"},
+                       {"key": "brdim", "value": "0,0,0,0,1536,0,1536,834,686,731"},
+                       {"key": "vis", "value": "1"},
+                       {"key": "wgl", "value": "true"},
+                       {"key": "ca_type", "value": "image"}]
+        },
+        "clickTracking": {
+            "clickTrackingParams": "CBAQui8iEwibm9OEjZL9AhVSSw8CHRVrC7I="
+        },
+        "client": {
+            "acceptHeader": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+            "browserName": "Chrome",
+            "browserVersion": "103.0.0.0",
+            "clientFormFactor": "UNKNOWN_FORM_FACTOR",
+            "clientName": "WEB",
+            "clientVersion": "2.20230206.06.00",
+            "configInfo": {
+                "appInstallData": "CL_vp58GEOf3rgUQgt2uBRCU-K4FELbgrgUQuNSuBRCH3a4FEMzfrgUQov-uBRCjgK8FEInorgUQouyuBRDi1K4FEOWg_hIQsKT-EhCZ6q4FEP7urgUQioCvBRDa6a4FELac_hIQuIuuBRDM9a4FEPDlrgUQm6f-EhCR-PwS"
+            },
+            "deviceExperimentId": "ChxOekU1T1RVMU56ZzRNVEF4TXpVM01UZ3pNQT09EL_vp58GGL_vp58G",
+            "deviceMake": "",
+            "deviceModel": "",
+            "gl": "HK",
+            "hl": "zh-CN",
+            "mainAppWebInfo": {
+                "graftUrl": "https://www.youtube.com/results?search_query=%E5%A5%B3%E5%9B%A2",
+                "isWebNativeShareAvailable": "true",
+                "pwaInstallabilityStatus": "PWA_INSTALLABILITY_STATUS_CAN_BE_INSTALLED",
+                "webDisplayMode": "WEB_DISPLAY_MODE_BROWSER"
+            },
+            "memoryTotalKbytes": "8000000",
+            "originalUrl": "https://www.youtube.com/results?search_query=%E7%81%AD%E5%9B%BD",
+            "osName": "Windows",
+            "osVersion": "10.0",
+            "platform": "DESKTOP",
+            "remoteHost": "219.76.157.82",
+            "screenDensityFloat": "1.25",
+            "screenHeightPoints": "731",
+            "screenPixelDensity": "1",
+            "screenWidthPoints": "686",
+            "timeZone": "Asia/Shanghai",
+            "userAgent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36,gzip(gfe)",
+            "userInterfaceTheme": "USER_INTERFACE_THEME_LIGHT",
+            "utcOffsetMinutes": "480",
+            "visitorData": "CgtZYWRXSk4ySl9JQSi_76efBg%3D%3D"
+        },
+        "request": {
+            "consistencyTokenJars": [],
+            "internalExperimentFlags": [],
+            "useSsl": "true"
+        },
+        "user": {
+            "lockedSafetyMode": "false"
+        },
+    },
+    "continuation": "EusDEgblpbPlm6Ia4ANTQlNDQVNKUVRHeHdMV0l6UkVkaFQzbG5lRlk0UmpsaE5IbHFXamRHWjFKMk1tRjNibXhJZ2dFTGRVSTBiRmxhZEV4MU4wR0NBU0pRVEVVNFJqZEplSFZxVTFwcWQzbGtiMDVaUkdaNmRVTTFVbmx4Y0RCNWFrbHBnZ0VMU3pNdFNsWklTV2xGZGttQ0FRdGlVM0F3TkhnMVpsZE1XWUlCQ3pRNVpEazFibWsxU2psWmdnRUxUak10WkVKWlVUUklUbG1DQVFzdFMydElla1UwY2s0NFo0SUJJbEJNV21kdVMzZHRVRFU1WW5sc1JXRjJhVlpOYm1keVp5MXJPV2xxZDJwa2RVV0NBUXRoVlU0eWRWQm5Xa05GWTRJQkMwVkJWRlJtUldoblRsRmpnZ0VMYWtaWFVGOXVVVkUzWWppQ0FRdFFPVVJFVFZwdWFUVlhXWUlCQzI1M1VFNURhWEpCTVhsemdnRUxUWEJ0WWtSdk5qWlViV2VDQVF0eGJsODBWVGhyVjBaTmQ0SUJDMmhVYVRKVFYzZE1RMDkzZ2dFTGJqZG9ObFp0WlhaSFYzZUNBUXQwVTJZeVNFY3lOVWszUVlJQkMyMU1XRW8yU0hsTWMwcEJzZ0VHQ2dRSUZ4QUMYgeDoGCILc2VhcmNoLWZlZWQ%3D"
+}
